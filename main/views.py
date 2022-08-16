125d6ab850ab68ab881559fccdfd977025777941
Abdukarim2 <abdukarimhakimov461@gmail.com>
2022-08-16 14:45:23 +0500

    new commit

--- main/views.py
+++ main/views.py
@@ -3,8 +3,12 @@ from django.http import JsonResponse
 from .models import Course, Videos
 from .forms import CourseUserForm
 from django.views.decorators.csrf import csrf_exempt
-
-
+import telebot
+bot = telebot.TeleBot(token='1720356473:AAHYnusESxOqJc2PRXSlsNUkr8slIh6Vs-8')
+courses_list = ["Bolalar uchun dasturlash", 
+                "Arduino", "Kompyuter fani",
+                "Grafik dizayn", "WEB dasturlash (frontend)",
+                "WEB dasturlash (backend)"]
 
 def home(request):
     context = {
@@ -33,9 +37,19 @@ def api_register(request):
             try:
                 form.save()
                 data['status'] = 200
+                #bot
+                # fullname = form.data.get("full_name")
+                # age = form.data.get("age")
+                # phonenumber = form.data.get("number")
+                # course = courses_list[int(form.data.get("course"))-1]
+                # messageCheck = form.data.get("message") 
+                # message = messageCheck  if messageCheck  else "Habar yo'q"
+                # bot.send_message(-1001383923135, f"""Yangi o'quvchi\n\nIsmi-familyasi:\n{fullname}\n\nYoshi:\n{age}\n\nTelefo'n raqami:\n{phonenumber}\n\nTanlagan kursi:\n{course}\n\nQo'shimcha habar:\n{message}""")
+                bot.send_message(-1001383923135, "test")
+                #/bot
             except Exception as e:
                 data['error'] = e
         else:
             data['status'] = 500
-    return JsonResponse(data)
+    return JsonResponse(data, safe=False)
 
