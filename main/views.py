426d3f9e63954ddc9bd177b3d26806ce57e22639
Abdukarim2 <abdukarimhakimov461@gmail.com>
2022-08-27 08:17:29 +0500

    new commit

--- main/views.py
+++ main/views.py
@@ -4,7 +4,7 @@ from .models import Course, Videos
 from .forms import CourseUserForm
 from django.views.decorators.csrf import csrf_exempt
 import telebot
-bot = telebot.TeleBot(token='1720356473:AAHYnusESxOqJc2PRXSlsNUkr8slIh6Vs-8')
+bot = telebot.TeleBot(token='')
 courses_list = ["Bolalar uchun dasturlash", 
                 "Arduino", "Kompyuter fani",
                 "Grafik dizayn", "WEB dasturlash (frontend)",
@@ -38,14 +38,13 @@ def api_register(request):
                 form.save()
                 data['status'] = 200
                 #bot
-                # fullname = form.data.get("full_name")
-                # age = form.data.get("age")
-                # phonenumber = form.data.get("number")
-                # course = courses_list[int(form.data.get("course"))-1]
-                # messageCheck = form.data.get("message") 
-                # message = messageCheck  if messageCheck  else "Habar yo'q"
-                # bot.send_message(-1001383923135, f"""Yangi o'quvchi\n\nIsmi-familyasi:\n{fullname}\n\nYoshi:\n{age}\n\nTelefo'n raqami:\n{phonenumber}\n\nTanlagan kursi:\n{course}\n\nQo'shimcha habar:\n{message}""")
-                bot.send_message(-1001383923135, "test")
+                fullname = form.data.get("full_name")
+                age = form.data.get("age")
+                phonenumber = form.data.get("number")
+                course = courses_list[int(form.data.get("course"))-1]
+                messageCheck = form.data.get("message") 
+                message = messageCheck  if messageCheck  else "Habar yo'q"
+                bot.send_message(-1001383923135, f"""Yangi o'quvchi\n\nIsm-familyasi:\n{fullname}\n\nYoshi:\n{age}\n\nTelefo'n raqami:\n+{phonenumber}\n\nTanlagan kursi:\n{course}\n\nQo'shimcha habar:\n{message}""")
                 #/bot
             except Exception as e:
                 data['error'] = e
