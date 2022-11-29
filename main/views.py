846b1307b2e03e6890c3a50b8662c7cafb5a2016
Abdukarim2 <abdukarimhakimov461@gmail.com>
2022-11-29 14:58:02 +0500

    new commit

--- main/views.py
+++ main/views.py
@@ -5,7 +5,12 @@ from .models import Course, Videos
 from .forms import CourseUserForm
 from django.views.decorators.csrf import csrf_exempt
 import telebot
-bot = telebot.TeleBot(token='')
+
+BOT_TOKEN = ''
+
+GROUP_ID = None
+
+bot = telebot.TeleBot(token=BOT_TOKEN)
 courses_list = ["Bolalar uchun dasturlash", 
                 "Arduino", "Kompyuter fani",
                 "Grafik dizayn", "WEB dasturlash (frontend)",
@@ -45,7 +50,7 @@ def api_register(request):
                 course = courses_list[int(form.data.get("course"))-1]
                 messageCheck = form.data.get("message") 
                 message = messageCheck  if messageCheck  else "Habar yo'q"
-                bot.send_message(-1001383923135, f"""Yangi o'quvchi\n\nIsm-familyasi:\n{fullname}\n\nYoshi:\n{age}\n\nTelefo'n raqami:\n+{phonenumber}\n\nTanlagan kursi:\n{course}\n\nQo'shimcha habar:\n{message}""")
+                bot.send_message(GROUP_ID, f"""Yangi o'quvchi\n\nIsm-familyasi:\n{fullname}\n\nYoshi:\n{age}\n\nTelefo'n raqami:\n+{phonenumber}\n\nTanlagan kursi:\n{course}\n\nQo'shimcha habar:\n{message}""")
                 #/bot
             except Exception as e:
                 data['status'] = None
